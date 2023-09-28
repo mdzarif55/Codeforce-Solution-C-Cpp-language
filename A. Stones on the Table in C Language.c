85cea8ff3e14d2ef44cd92da058257dfc917e372
MD Muhtasim Zarif <146372499+mdzarif55@users.noreply.github.com>
2023-09-29 00:00:37 +0600

    A. Stones on the Table in C Language.c

--- A. Stones on the Table in C Language.c
+++ A. Stones on the Table in C Language.c
@@ -1,40 +1,16 @@
 #include<stdio.h>
 int main()
- 
 {
-    int n,i,a=0,b=0,c=0;
-    scanf("%d\n",&n);
- 
-    char s[n];
- 
-    gets(s);
- 
-    for ( i=0; i<n;i++ )
+    int a,b=0;
+    scanf("%d",&a);
+    char s[a];
+    scanf("%s",s);
+    for(int i=0;i<a;i++)
     {
- 
-        if ( s[i]=='R')  while(s[i+1]=='R')
-        {
- 
-            a++;
-            i++;
-        }
- 
-         if ( s[i]=='G')  while(s[i+1]=='G')
-        {
- 
+        if(s[i]==s[i+1])
             b++;
-            i++;
-        }
- 
-         if ( s[i]=='B')  while(s[i+1]=='B')
-        {
- 
-            c++;
-            i++;
-        }
     }
-  
-    printf("%d\n",a+b+c);
- 
+    printf("%d\n",b);
+
     return 0;
 }
