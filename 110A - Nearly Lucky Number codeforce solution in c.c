54b8d88b454fbd7061af93d02356d45cf36b262e
MD Muhtasim Zarif <146372499+mdzarif55@users.noreply.github.com>
2023-10-07 01:16:32 +0600

    110A - Nearly Lucky Number codeforce solution in c.c

---
+++ 110A - Nearly Lucky Number codeforce solution in c.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include <string.h>
+int main()
+{
+    long long a;
+    int n,c=0;
+    scanf("%lld",&a);
+     for ( ;a != 0;)
+//while(a!=0)
+    {
+
+        if(a%10==7 ||a%10==4)
+            c++;
+            a=a/10;
+    }
+    if(c==4 || c==7)
+        printf("YES");
+    else
+        printf("NO");
+
+    return 0;
+}
+
